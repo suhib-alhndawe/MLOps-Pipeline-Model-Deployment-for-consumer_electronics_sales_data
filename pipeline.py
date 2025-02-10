f276e349fe646f8ba248703d6daa92b4b4b59f62
suhib-alhndawe <101812035+suhib-alhndawe@users.noreply.github.com>
2025-02-11 00:25:00 +0300

    pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -25,8 +25,8 @@ df.drop('ProductID', axis=1, inplace=True)
 # =========================
 # FEATURE ENGINEERING
 # =========================
-df['PricePerUnit'] = df['ProductPrice'] / df['PurchaseFrequency']  # Example of a new feature
-df['Age_Category'] = pd.cut(df['CustomerAge'], bins=[0, 18, 35, 50, 100], labels=[0, 1, 2, 3])  # Age category
+df['PricePerUnit'] = df['ProductPrice'] / df['PurchaseFrequency']  
+df['Age_Category'] = pd.cut(df['CustomerAge'], bins=[0, 18, 35, 50, 100], labels=[0, 1, 2, 3]) 
 
 # =========================
 # SPLIT DATA
@@ -44,14 +44,11 @@ pipeline = Pipeline([
     ('model', RandomForestClassifier(max_depth=10, random_state=0))  # Random Forest classifier
 ])
 
-# Train the model using the pipeline
 pipeline.fit(X_train, y_train)
 
-# Save the trained model and encoders
 joblib.dump(pipeline, 'purchase_intent_model.pkl')
 joblib.dump(le_category, 'le_category.pkl')  # Save the LabelEncoder for ProductCategory
 joblib.dump(le_brand, 'le_brand.pkl')  # Save the LabelEncoder for ProductBrand
 
-# Optionally: Evaluate the model on the test set
 test_accuracy = pipeline.score(X_test, y_test)
 print(f"Test Accuracy: {test_accuracy}")
